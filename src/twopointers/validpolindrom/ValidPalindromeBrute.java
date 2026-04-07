f3ca341fdcfd9276f66dd27bc835d4a9e1a37c49
Vertefil <arhipiov.alex@bk.ru>
2026-04-07 22:21:22 +0300

    ValidPalindrome

---
+++ src/twopointers/validpolindrom/ValidPalindromeBrute.java
@@ -0,0 +1,32 @@
+package twopointers.validpolindrom;
+
+public class ValidPalindromeBrute {
+    /**
+     * На вход подаётся строка, нужно узнать является ли она палиндромом?
+     *
+     * Идея: так как строка может быть любая, но нам интересны, только буквы и цифры, первой задачей
+     * будет приведение строки к нижнему регистру и формату: буква или цифра.
+     * После запускаем два указателя на начало и конец и сравниваем посимвольно доходя до середины слова.
+     *
+     * Сложность O(n) память O(n).
+     *
+     * @param s String
+     */
+    public static boolean isPalindrome(String s) {
+        StringBuilder sb = new StringBuilder();
+        for(char c : s.toCharArray()) {
+            if(Character.isLetterOrDigit(c)) sb.append(Character.toLowerCase(c));
+        }
+        if(sb.isEmpty()) return true;
+        int l = 0, r = sb.length()-1;
+        // 6 n = 3 1..2..3   6..5..4
+        for(int i = 0; i < (sb.length() / 2) + 1;i++) {
+            if(sb.charAt(l) != sb.charAt(r)){
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+}
---
+++ src/twopointers/validpolindrom/ValidPalindromeOptimal.java
@@ -0,0 +1,30 @@
+package twopointers.validpolindrom;
+
+public class ValidPalindromeOptimal {
+    /**
+     * На вход подаётся строка, нужно узнать является ли она палиндромом?
+     *
+     * Идея: вешаем два указателя на начало и конец, двигаем указатели, пока символ не является буквой или цифрой.
+     * Если символ по указателям буква или цифра, приводим к нижнему регистру и сравниваем.
+     *
+     * Сложность O(n) память O(1) используется только набор данных.
+     *
+     * @param s String
+     */
+    public static boolean isPalindrome(String s) {
+        if(s.length() == 1) return true;
+        int l = 0, r = s.length()-1;
+        while(l < r) {
+            while(l < r && !Character.isLetterOrDigit(s.charAt(l))) {
+                l++;
+            }
+            while(r > l && !Character.isLetterOrDigit(s.charAt(r))) {
+                r--;
+            }
+            if(Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r))) return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+}
